87b1270b390f0654d5666b508a9574b31c97cc59
rgr-myrg <rpmyrg@gmail.com>
2011-12-01 12:54:33 -0500

    Adding Event signal pattern

---
+++ src/main/js/patterns/Event.js
@@ -0,0 +1,27 @@
+(function(BTG){
+	BTG.Event = function(obj){
+		var listeners = [];
+		this.add = function(listener){
+			if(typeof listener === 'function'){
+				listeners.push(listener);
+			}
+		};
+		this.remove = function(listener){
+			var size = listeners.length;
+			for(var x=0; x < size; x++){
+				if(listeners[x] === listener){
+					listeners[x] = function(){return null;};
+				}
+			}
+		};
+		this.dispatch = function(){
+			var size = listeners.length;
+			for(var x=0; x < size; x++){
+				try{
+					listeners[x].apply(this,arguments);
+				}catch(e){
+				}
+			}
+		};
+	};
+})(BTG);
