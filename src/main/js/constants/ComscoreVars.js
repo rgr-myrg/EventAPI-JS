5e86ec856455b745277c7b5323003387f35a5867
rgr-myrg <rpmyrg@gmail.com>
2011-12-08 17:03:49 -0500

    Added constants package

---
+++ src/main/js/constants/ComscoreVars.js
@@ -0,0 +1,9 @@
+(function(BTG){
+	BTG.ComscoreVars = {
+		BEACON_URL      : "http://beacon.securestudies.com/scripts/beacon.dll",
+		BEACON_TYPEID   : "1",
+		VIDEAD_TITLE    : "VIDEO_AD",
+		VIDEOAD_GENREID : "01",
+		CONTENT_GENREID : "03"
+	};
+})(BTG);
